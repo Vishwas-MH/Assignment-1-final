86db8616df995695ac8b213daa7c1e019195bdfd
Vishwas-MH <91179257+Vishwas-MH@users.noreply.github.com>
2021-09-30 09:39:10 +0530

    Updated pagination, landscape mode, transitions

--- js/script.js
+++ js/script.js
@@ -95,3 +95,132 @@ function debounce(func, timeout = 600){
 }
 
 const SearchDelay = debounce(() => SearchTable());
+
+
+let tableElement = document.getElementById("table-sortable");
+let tableBody = tableElement.tBodies[0];
+let tr = Array.from(tableBody.querySelectorAll("tr"));
+let ul = document.querySelector("ul");
+var arrayTr = [];
+for (let i = 0; i < tr.length; i++) {
+  arrayTr.push(tr[i]);
+}
+
+let limit = 5;
+
+function displayTable(limit) {
+  tableBody.innerHTML = "";
+  for (let i = 0; i < limit; i++) {
+    tableBody.appendChild(arrayTr[i]);
+  }
+  buttonGereration(limit);
+}
+
+function buttonGereration(limit) {
+  var noftr = arrayTr.length;
+  if (noftr <= limit) {
+    ul.style.display = "none";
+  } else {
+    ul.style.display = "flex";
+    var nofPage = Math.ceil(noftr / limit);
+    updatepage(1);     
+    var updateflagpage=0;
+    for (let i = 1; i <= nofPage; i++) {
+
+      
+      let li = document.createElement("li");
+      li.className = "list";
+      let a = document.createElement("a");
+      a.href = "#";
+      a.setAttribute("data-page", i);
+      a.setAttribute("id", "datapage"+i);
+      if (updateflagpage == 0){
+        updateflagpage = 1;        
+        a.setAttribute("class", "activepage");
+      }
+      li.appendChild(a);
+      a.innerText = i;
+      ul.insertBefore(li, ul.querySelector(".nextId"));
+      ul.insertBefore(li, document.getElementById("tableList").childNodes[3]);
+
+      a.onclick = (e) => {
+        let x = e.target.getAttribute("data-page");
+        updatepage(x);  
+        tableBody.innerHTML = "";
+        x--;
+        let start = limit * x;
+        let end = start + limit;
+        let page = arrayTr.slice(start, end);
+
+        for (let i = 0; i < page.length; i++) {
+          let item = page[i];
+          tableBody.appendChild(item);
+        }
+      };
+    }
+  }
+  var z = 0, flag=0;
+  function nextElement() {
+    if (this.id == "nextId") {
+      flag=1;
+      console.log(z);
+      z == arrayTr.length - limit ? (z = 0) : z/limit+1 == nofPage ? z : (z += limit);
+    }
+
+    if (this.id == "prevId") {
+      flag=1;
+      console.log(z);
+      z == 0 ? arrayTr.length - limit : (z -= limit);
+    }
+    updatepage(z/limit+1);  
+
+    tableBody.innerHTML = "";
+    for (let i = z; i < z+limit; i++) {
+      if(arrayTr[i] != null){
+        tableBody.appendChild(arrayTr[i]);
+      }
+      else{
+        break;
+      }
+    }
+  }
+
+  document.getElementById("prevId").onclick = nextElement;
+  document.getElementById("nextId").onclick = nextElement;
+}
+
+displayTable(5);
+
+function onChangeGoToPage(go){
+  var noftr = arrayTr.length;
+  var nofPage = Math.ceil(noftr / limit);
+  if(go <= nofPage && go >0){
+    
+    var goto = go - 1;    
+    updatepage(go);  
+    if(nofPage < goto){
+      console.log("invalid go to");
+      return;
+    }
+    let offset = (goto * limit);
+    tableBody.innerHTML = "";
+    for (let i = offset; i < (offset+limit); i++) {
+      if(arrayTr[i] != null){
+        tableBody.appendChild(arrayTr[i]);
+      }
+      else{
+        break;
+      }
+    }
+  }
+}
+const GoToPage = debounce((go) => onChangeGoToPage(go));
+
+function updatepage(go){
+
+  var spanvalue1 = document.getElementById("goto_lower");
+  spanvalue1.textContent = (go*limit+1 - limit);
+  
+  var spanvalue2 = document.getElementById("goto_upper");
+  spanvalue2.textContent = (go*limit);
+}
